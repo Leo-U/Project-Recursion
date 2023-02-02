6265173be3e7eb77995aa1ebff4851be45aafd97
Joel Howard <joelhoward2@gmail.com>
2023-02-02 14:11:18 -0500

    edit top comment

--- recursion_project.rb
+++ recursion_project.rb
@@ -1,4 +1,4 @@
-# 1. Write a recursive method #fibs which akes a number and returns an array containing that many numbers from the fibonacci sequence. Using an example input of `8`, this method should return the array `[0, 1, 1, 2, 3, 5, 8, 13]`.
+# 1. Write a recursive method #fibs which akes a number and returns an array containing that many numbers from the fibonacci sequence.
 
 def fibs(n, a = 0, b = 1, result = [0])
   return result if n == 1
