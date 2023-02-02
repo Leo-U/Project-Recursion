7341de5707b196d53d9290b2b4eb54464de730e5
Joel Howard <joelhoward2@gmail.com>
2023-02-02 11:19:47 -0500

    finish merge_sort

--- recursion_project.rb
+++ recursion_project.rb
@@ -5,3 +5,28 @@ def fibs(n, a = 0, b = 1, result = [0])
   result << b
   fibs(n - 1, b, a + b, result)
 end
+
+# 2. Make a method that uses Merge Sort and recursion to sort a given array.
+
+def merge_sort(array)
+  return array if array.length < 2
+
+  mid = array.length / 2
+  left_half = array.slice(0, mid)
+  right_half = array.slice(mid, array.length - mid)
+
+  left = merge_sort(left_half)
+  right = merge_sort(right_half)
+  
+  merged = []
+
+  while left.length > 0 && right.length > 0 do
+    if left[0] <= right[0]
+      merged << left.shift
+    elsif left[0] > right[0]
+      merged << right.shift
+    end    
+  end
+
+  return merged + left + right
+end
